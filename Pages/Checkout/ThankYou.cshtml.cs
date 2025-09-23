bd3dddbfd0779caa37bb9c31e4f81d3095d522d8
Your Name <you@example.com>
2025-09-23 15:57:15 +0300

    Pushaus 2 Powershell:stä

---
+++ Pages/Checkout/ThankYou.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MarkanPizzeria.Pages.Checkout
+{
+    public class ThankYouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
