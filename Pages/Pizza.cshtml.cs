9e12d5277c2ba5cc8cf9077c0c1a99c33d7a8389
Your Name <you@example.com>
2025-09-24 22:02:14 +0300

    Pushaus 7 Powershell:stä

--- Pages/Pizza.cshtml.cs
+++ Pages/Pizza.cshtml.cs
@@ -1,12 +1,20 @@
-using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MarkanPizzeria.Models;  
 
 namespace MarkanPizzeria.Pages
 {
     public class PizzaModel : PageModel
     {
-        public void OnGet()
+        public List<PizzasModel> fakePizzaDB { get; } = new()
         {
-        }
+            new PizzasModel { ImageTitle = "Margerita", PizzaName = "Margerita", BasePrice = 2, TomatoSauce = true, Cheese = true, FinalPrice = 4 },
+            new PizzasModel { ImageTitle = "Bolognese", PizzaName = "Bolognese", BasePrice = 2, TomatoSauce = true, Cheese = true, FinalPrice = 4 },
+            new PizzasModel { ImageTitle = "Seafood",   PizzaName = "Seafood",   BasePrice = 2, TomatoSauce = true, Cheese = true, FinalPrice = 4 },
+            
+        };
+
+        public void OnGet() { }
     }
 }
+
