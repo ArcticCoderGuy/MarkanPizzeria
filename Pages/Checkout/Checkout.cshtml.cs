4299769033c8057ec57ed511e419a8f6b718e1b9
Your Name <you@example.com>
2025-09-24 12:41:06 +0300

    Pushaus 5 Powershell:stä kun korjattu toi Pizza etusivu

--- Pages/Checkout/Checkout.cshtml.cs
+++ Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MarkanPizzeria.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+        public string PizzaName { get; set; } = string.Empty;
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; } = string.Empty;
+
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
         }
     }
 }
