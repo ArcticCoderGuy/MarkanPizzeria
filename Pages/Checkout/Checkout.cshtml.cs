c9dd0d7bdbcec8656cda8868fb7d45bb5fdd7cab
Your Name <you@example.com>
2025-10-03 20:01:15 +0300

    Pushaus 9 Powershell:stä

--- Pages/Checkout/Checkout.cshtml.cs
+++ Pages/Checkout/Checkout.cshtml.cs
@@ -1,26 +1,85 @@
+
+
+using MarkanPizzeria.Data;
+using MarkanPizzeria.Models;
 using Microsoft.AspNetCore.Mvc;
+
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using MarkanPizzeria.Data;
+
+using MarkanPizzeria.Models;
+
+
+
 namespace MarkanPizzeria.Pages.Checkout
+
 {
+
     [BindProperties(SupportsGet = true)]
+
     public class CheckoutModel : PageModel
+
     {
-        public string PizzaName { get; set; } = string.Empty;
+
+        public string PizzaName { get; set; }
+
         public float PizzaPrice { get; set; }
-        public string ImageTitle { get; set; } = string.Empty;
+
+        public string ImageTitle { get; set; }
+
+
+
+        private readonly ApplicationDbContext _context;
+
+
+
+        public CheckoutModel(ApplicationDbContext context)
+
+        {
+
+            _context = context;
+
+        }
+
+
 
         public void OnGet()
+
         {
+
             if (string.IsNullOrWhiteSpace(PizzaName))
+
             {
+
                 PizzaName = "Custom";
+
             }
 
             if (string.IsNullOrWhiteSpace(ImageTitle))
+
             {
+
                 ImageTitle = "Create";
+
             }
+
+
+
+            PizzaOrder pizzaOrder = new PizzaOrder();
+
+            pizzaOrder.PizzaName = PizzaName;
+
+            pizzaOrder.BasePrice = PizzaPrice;
+
+
+
+            _context.PizzaOrders.Add(pizzaOrder);
+
+            _context.SaveChanges();
+
         }
+
     }
+
 }
